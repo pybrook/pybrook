ccd6ddfa7f2273db01f2be845425c79fb8bfc89c
Michał Rokita <mrokita@mrokita.pl>
2023-03-15 13:10:33 +0100

    chore: add license

--- pybrook/__init__.py
+++ pybrook/__init__.py
@@ -1,7 +1,24 @@
 """This module contains package metadata"""
 
+#  PyBrook
+#
+#  Copyright (C) 2023  Michał Rokita
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 try:
-    import importlib.metadata as importlib_metadata  # noqa: WPS301
+    import importlib.metadata as importlib_metadata
 except ModuleNotFoundError:  # pragma: no cover
     import importlib_metadata  # type: ignore # noqa: WPS440
 
--- pybrook/models.py
+++ pybrook/models.py
@@ -3,6 +3,38 @@ This module is responsible for parsing report processing models.
 
 It is the core of PyBrook.
 """
+#  PyBrook
+#
+#  Copyright (C) 2023  Michał Rokita
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 import asyncio
 import dataclasses
 import inspect
